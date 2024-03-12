7dd5fa1638a22601c1a95d18277433855f527241
Jaco56 <156460524+Jaco56@users.noreply.github.com>
2024-03-11 23:23:47 -0600

    Update index.js
    fixed index.js for deplyment

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const apiRouter = express.Router();
 
-app.use(express.static(path.join(__dirname)));
-app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(express.json());
 
 const users = [
     { username: 'jaco', password: 'password1', squatMax: '500 Lbs', benchMax: '405 lbs', deadliftMax: '505 Lbs' },
     { username: 'user2', password: 'password2', squatMax: '400 Lbs', benchMax: '300 Lbs', deadliftMax: '450 Lbs' }
 ];
 
-app.put('/api/login', (req, res) => {
+apiRouter.put('/login', (req, res) => {
     const { username, password } = req.body;
     
     const user = users.find(user => user.username === username && user.password === password);
@@ -24,7 +25,7 @@ app.put('/api/login', (req, res) => {
     }
 });
 
-app.get('/api/user/maxes', (req, res) => {
+apiRouter.get('/user/maxes', (req, res) => {
     const loggedInUsername = req.query.username;
 
     const user = users.find(user => user.username === loggedInUsername);
@@ -39,7 +40,11 @@ app.get('/api/user/maxes', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use('/api', apiRouter);
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
