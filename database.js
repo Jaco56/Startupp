f0b7360f671c9bd531e992d9efa0fbe43dd00a16
Jaco56 <156460524+Jaco56@users.noreply.github.com>
2024-03-22 13:07:57 -0600

    Update database.js
    connection to mongodb and functions defined for login and creating user

--- database.js
+++ database.js
@@ -1,1 +1,51 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const config = require('./dbConfig.json');
 
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const dbName = 'bulk_buddy';
+const userCollectionName = 'users';
+
+async function initializeDB() {
+    try {
+        await client.connect();
+        await client.db().command({ ping: 1 });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error(`Unable to connect to database with ${url} because ${error.message}`);
+        process.exit(1);
+    }
+}
+
+async function createUser(username, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = {
+        username: username,
+        password: hashedPassword
+    };
+    const db = client.db(dbName);
+    const collection = db.collection(userCollectionName);
+    await collection.insertOne(newUser);
+}
+
+async function loginUser(username, password) {
+    const db = client.db(dbName);
+    const collection = db.collection(userCollectionName);
+    const user = await collection.findOne({ username: username });
+    if (!user) {
+        return null; // User not found
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+        return null; // Incorrect password
+    }
+    return user; // Return user data if login successful
+}
+
+
+module.exports = {
+    initializeDB,
+    createUser,
+    loginUser,
+};
